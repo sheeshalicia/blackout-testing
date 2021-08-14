aab9ddf118b43efe099adfebff0cdc9ed2504539
Alicia C <73862079+sheeshalicia@users.noreply.github.com>
2021-08-14 18:42:10 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+//change the "show" class to "hide" when hovered over
+//ask user for a word they'd like to show in a sentence
+//create a prompt that get the user to write a sentence using word
+//turn the inputted string of words into an array of single words
+///do we also need to turn them all lower-case,etc?
+//remove the word you want to show from the array
+//create a listener/handler that changes the background-color once hovered (is this possible? want to keep it until refreshed)
+///then build it out to multiple words like a poem within a series of sentences/paragraph
